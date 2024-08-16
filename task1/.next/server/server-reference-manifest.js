7099b53f6ab2587fba3110a2f6958fbc8c208289
Abdullah-Farhan <102550496+Abdullah-Farhan@users.noreply.github.com>
2024-08-16 00:57:55 -0700

    first commit

---
+++ task1/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nesMqyLLKD4wXqu2xkoyT11wzu34jsa5RqXQdprlhK4=\"\n}"
---
+++ testfile/librarytest/app/page.js
@@ -0,0 +1,11 @@
+"use client"
+import { Table, Tables } from "fliegen-library";
+import 'fliegen-library/dist/styles.css'
+
+export default function Home() {
+  return (
+    <>
+    <Table/>
+    </>
+  );
+}
